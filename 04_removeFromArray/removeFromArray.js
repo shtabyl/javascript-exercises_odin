c6911240d8cad156e6025bbed032673843816a06
shtabyl <avnirog@gmail.com>
2024-05-25 11:38:37 +0300

    Test function removeFromArray, fix bugs

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = (array, ...argsToRemove) => {
+    argsToRemove.forEach(arg => {
+        while (array.includes(arg)) {
+            array.splice(array.indexOf(arg), 1);
+        }
+    });
+    return array;
 };
 
 // Do not edit below this line
