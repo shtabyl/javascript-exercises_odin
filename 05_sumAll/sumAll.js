6c3a2f5ecf7acf9b8df27f1b226cc43822c6fb63
shtabyl <avnirog@gmail.com>
2024-05-25 11:56:24 +0300

    Test sumAll(), fix bugs

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0) {
+        return 'ERROR';
+    }
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        return 'ERROR';
+    }
 
+    // Gauss's formula insensitive to min or max number
+    return (start + end) * (Math.abs(end - start) + 1) / 2;
 };
 
 // Do not edit below this line
