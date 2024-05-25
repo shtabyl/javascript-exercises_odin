c24d82ccedd888c92265fcba10474c04b315060f
shtabyl <avnirog@gmail.com>
2024-05-25 09:59:13 +0300

    Test function repeatString, fix bugs

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = (string, times) => {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    return string.repeat(times);
 };
 
 // Do not edit below this line
