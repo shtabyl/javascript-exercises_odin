c83a664678909ec7f3869a5139b87b0b0b063e14
shtabyl <avnirog@gmail.com>
2024-06-02 23:37:25 +0300

    Fix findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.forEach(person => {
+        if (person.yearOfDeath) {
+            person['age'] = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            let currentTime = new Date();
+            person['age'] = currentTime.getFullYear() - person.yearOfBirth;
+        }
+    });
 
+    const ages = people.map(person => person.age);
+    const oldest = ages.reduce((a, b) => Math.max(a, b), -Infinity);
+    return people.find(person => person.age === oldest);
 };
 
 // Do not edit below this line
