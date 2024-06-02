5f26e27a5cb0a790ccbc6fd5658258d6f19bc9e3
shtabyl <avnirog@gmail.com>
2024-06-02 22:13:44 +0300

    Fix palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const strLettersOnly = str.replace(/[^\p{L}\p{N}]/gu, '').toLowerCase();
+    return Array.from(strLettersOnly).reverse().join('') === strLettersOnly;
 };
 
 // Do not edit below this line
