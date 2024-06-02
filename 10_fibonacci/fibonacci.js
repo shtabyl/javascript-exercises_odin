4c188fa739f1497370290e7c72a40a66deac7eb8
shtabyl <avnirog@gmail.com>
2024-06-02 22:47:38 +0300

    Fix fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num == 0) {
+        return 0;
+    }
+    if (num == 1) {
+        return 1;
+    }
+    if (num < 0) {
+        return 'OOPS';
+    }
+    let a = 0;
+    let b = 1;
+    let sum = 0;
+    for (let i = 1; i < num; i++) {
+        sum = a + b;
+        a = b;
+        b = sum;
+    }
+    return sum;
 };
 
 // Do not edit below this line
